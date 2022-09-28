d55af137523c880ddbf7f92d14013477d3a247af
ElenaMitrofan <114084990+ElenaMitrofan@users.noreply.github.com>
2022-09-28 16:34:48 +0300

    Домашнее задание - 2

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Домашнее задание - 2");
         // Задание 1
-        int age = 200;
+        int age = 20;
         if (age >=18) {
             System.out.println("Поздравляем с совершенолетием!");
-        }
-        if (age <18) {
+        } else {
             System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать");
         }
 
         // Задание 2
         if (age >=7 && age < 18) {
             System.out.println("Ребенок ходит в школу");
-        }
-        if (age >=18 && age < 24) {
+        } else if (age >=18 && age < 24) {
             System.out.println("Человек уже закончил школу и может отправляться в университет");
-        }
-        if (age >=24) {
+        } else {
             System.out.println("Человек окончил университет и ему пора искать первую работу");
         }
-
         // Задание 3
         int capacity = 102;
         int seatPlace = 60;
@@ -29,11 +26,9 @@ public class Main {
 
         if (seatsUsed <= seatPlace) {
             System.out.println("Осталось " + (seatPlace - seatsUsed) + " сидячих мест и " + standingRoom + " стоячих мест");
-        }
-        if (seatsUsed > seatPlace && seatsUsed <= capacity) {
+        } else if (seatsUsed > seatPlace && seatsUsed <= capacity) {
             System.out.println("Осталось " + (capacity - seatsUsed) + " стоячих мест");
-        }
-        if (seatsUsed > capacity) {
+        } else {
             System.out.println("Вагон не может вместить такое количество пассажиров");
         }
 
